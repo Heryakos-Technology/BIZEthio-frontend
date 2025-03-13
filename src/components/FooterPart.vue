d06fb92cd55f596e24ab230529ee0410ccb286f9
Tiegist Girma <tigistgirma40@gmail.com>
2025-03-13 13:46:40 +0300

    landing page implementation including navbar and footer

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -1,32 +1,38 @@
 <template>
     <footer>
-        <div class=" bg-[#1B728C] text-gray-200 font-normal w-screen p-6 h-96 -mt-10 bottom-0">
-            <div class="flex">
-                <div class="w-56 h-40">
-                    <p class="text-center mt-4">COMPANY</p>
-                    <p class="text-center mt-4">about us</p>
-                    <p class="text-center mt-4">our services</p>
-                    <p class="text-center mt-4">contacts</p>
-                </div>
-                <div class="w-56 h-40 p-2 ml-6 mt-3">
-                    <p class="mb-4 text-center ml-4">CONTACT US</p>
-                    <div class="flex mt-6 w-32 mx-auto ">
-                        <i class="fa-solid fa-phone text-gray-700 mr-4 mt-1"></i>
-                        <p>+2519222222</p>
-                    </div>
-                    <div class="flex mt-6">
-                        <i class="fa-solid fa-envelope text-gray-700 mr-4 mt-2"></i>
-                        <p>fakeemail@gmail.com</p>
-                    </div>
-                </div>
+        <div class=" bg-[#1B728C] text-gray-200 font-normal w-screen p-6 h-96 lg:h-72 -mt-10 bottom-0">
+            <div class="lg:flex md:flex hidden  md:block">
+                <img src="/logo.png" alt="" class="w-20 mr-2">
+                <p class="mt-2">BIZEthio</p>
             </div>
-            <div class="font-light">
-                <p class="text-center mt-4">ADDRESS</p>
-                <p class="text-center mt-4">123 Tech Avenue</p>
-                <p class="text-center mt-4">Innovation City, IC 45678</p>
-                <p class="text-center mt-8">© 2025 Company Name. All rights reserved.</p>
+            <div class="lg:flex md:flex lg:mx-auto">
+                <div class="flex">
+                    <div class="w-56 h-40 lg:ml-64">
+                        <p class="text-center mt-4">COMPANY</p>
+                        <p class="text-center mt-4 lg:mt-6">about us</p>
+                        <p class="text-center mt-4 lg:mt-6">our services</p>
+                        <p class="text-center mt-4 lg:mt-6">contacts</p>
+                    </div>
+                    <div class="w-56 h-40 p-2 ml-6 mt-3 lg:ml-32">
+                        <p class="mb-4 text-center ml-4">CONTACT US</p>
+                        <div class="flex mt-6 w-32 mx-auto ">
+                            <i class="fa-solid fa-phone text-gray-700 mr-4 mt-1"></i>
+                            <p>+2519222222</p>
+                        </div>
+                        <div class="flex mt-6">
+                            <i class="fa-solid fa-envelope text-gray-700 mr-4 mt-2"></i>
+                            <p>fakeemail@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+                    <div class="font-light lg:ml-40 md:ml-20 md:font-normal lg:font-normal lg:mt-1.5">
+                        <p class="text-center mt-4">ADDRESS</p>
+                        <p class="text-center mt-4 lg:mt-6 md:mt-6">123 Tech Avenue</p>
+                        <p class="text-center mt-4 lg:mt-6 md:mt-6">Innovation City, IC 45678</p>
+                    </div>
+                </div>
+                <p class="text-center mt-8 lg:mt-4 md:font-extralight">© 2025 Company Name. All rights reserved.</p>
             </div>
-        </div>
     </footer>
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,12 +5,12 @@
            <div class=" ml-48"> <i class="fa-solid fa-bars  mt-6"></i></div>
         </div>
     </div>
-    <div class="hidden  md:block ">
-        <div class=" w-1/2 mx-auto p-1 rounded-full  h-14 bg-[#C8EDF8]">
+    <div class="hidden  md:block">
+        <div class=" w-1/2 md:w-2/3 mx-auto p-1 rounded-full  h-14 bg-[#C8EDF8]">
             <div class="flex">
             <div class="flex">
             <img src="\logo.png" alt="" class="w-24 ml-4 ">
-            <p class="mt-3 ml-2 text-lg">BIZEthio</p>
+            <p class="mt-3 ml-2 text-lg md:text-md">BIZEthio</p>
             </div>
             <div class="flex w-64 mx-auto mt-3">
                 <p class="mr-6">Home</p>
