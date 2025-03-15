44e24879dccdef93c3f0d60d4282e28dad1bbc1e
Tiegist Girma <tigistgirma40@gmail.com>
2025-03-15 20:59:28 +0300

    user profile completed implementation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,17 +9,17 @@
         <div class=" w-1/2 mx-auto p-1 rounded-full  h-14 bg-[#C8EDF8]">
             <div class="flex">
             <div class="flex">
-            <img src="\logo.png" alt="" class="w-24 ml-4 ">
-            <p class="mt-3 ml-2 text-lg">BIZEthio</p>
+            <img src="\logo.png" alt="" class="lg:w-24 lg:h-10 lg:mt-1 ml-4 md:w-16 md:h-8 md:mt-2">
+            <p class="lg:mt-2 ml-2 lg:text-lg md:text-[13px] md:font-semibold md:-ml-0.5 lg:mr-8">BIZEthio</p>
             </div>
-            <div class="flex w-64 mx-auto mt-3">
-                <p class="mr-6">Home</p>
-                <p class="mr-6">about us</p>
+            <div class="flex lg:w-64 md:w-40 md:ml-5 lg:mx-auto mx-auto mt-3 md:text-[12px] lg:text-[16px]">
+                <p class="lg:mr-6 md:mr-4">Home</p>
+                <p class="lg:mr-6 md:mr-4">about us</p>
                 <p>contacts</p>
             </div>
-            <div class="mt-1 -ml-8">
-                <i class="fa-solid fa-location-dot text-[#1B7590] mr-6 mt-2 text-xl"></i>
-                <i class="fa-solid fa-user mr-4 text-gray-800 text-xl"></i>
+            <div class="mt-1 lg:-ml-8 md:-ml-4">
+                <i class="fa-solid fa-location-dot text-[#1B7590] lg:mr-6 md:mr-4 md:text-sm mt-2 lg:text-xl"></i>
+                <i class="fa-solid fa-user mr-4 text-gray-800 md:text-text-sm lg:text-xl"></i>
             </div>
         </div>
         </div>
