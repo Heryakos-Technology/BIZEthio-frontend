7d5db4a63b9b1684e4123ebae55885b86d96310b
complatform <kalkidansol1623@gmail.com>
2025-03-19 22:24:29 +0300

    adjesting index js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 import RegistrationPage from '../views/RegistrationPage.vue'
@@ -6,8 +6,7 @@ import RegistrationPage from '../views/RegistrationPage.vue'
 import UserRegistration1 from '../views/Users/UserRegistration1.vue'
 import NextPage from '../views/Users/NextPage.vue'
 import SignIn from '../views/Users/SignIn.vue'
- 
-import AboutView from '@/views/AboutView.vue'
+
 import EditProfile from '@/views/userPage/EditProfile.vue'
 import ChangePassword from '@/views/userPage/ChangePassword.vue'
 import UserLanding from '@/views/userPage/UserLanding.vue'
@@ -16,7 +15,7 @@ import UserProfile from '@/views/userPage/UserProfile.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
