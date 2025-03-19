9e1d422ff3caaf78c7070a70ba1e7001a5af6072
Tiegist Girma <tigistgirma40@gmail.com>
2025-03-19 13:01:08 +0300

    fixing index route

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,35 @@
-import { createRouter, createWebHashHistory } from 'vue-router'; // Change to createWebHashHistory
-import HomeView from '../views/HomeView.vue';
-import RegistrationPage from '../views/RegistrationPage.vue';
-import UserRegistration1 from '../views/Users/UserRegistration1.vue';
-import NextPage from '../views/Users/NextPage.vue';
-import SignIn from '../views/Users/SignIn.vue';
-import EditProfile from '@/views/userPage/EditProfile.vue';
-import ChangePassword from '@/views/userPage/ChangePassword.vue';
-import UserLanding from '@/views/userPage/UserLanding.vue';
-import UserProfile from '@/views/userPage/UserProfile.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+import RegistrationPage from '../views/RegistrationPage.vue'
+
+import UserRegistration1 from '../views/Users/UserRegistration1.vue'
+import NextPage from '../views/Users/NextPage.vue'
+import SignIn from '../views/Users/SignIn.vue'
+// import AboutView from '@/views/AboutView.vue'
+import EditProfile from '@/views/userPage/EditProfile.vue'
+import ChangePassword from '@/views/userPage/ChangePassword.vue'
+import UserLanding from '@/views/userPage/UserLanding.vue'
+import UserProfile from '@/views/userPage/UserProfile.vue'
+
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL), 
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
       component: HomeView,
     },
+
     {
+
       path: '/registration',
       name: 'RegistrationPage',
       component: RegistrationPage,
     },
-    {
+    
+{
       path: '/signup',
       name: 'UserRegistration1',
       component: UserRegistration1,
@@ -42,6 +49,7 @@ const router = createRouter({
       name: 'UserLanding',
       component: UserLanding,
     },
+    
     {
       path: '/next',
       name: 'NextPage',
@@ -57,13 +65,13 @@ const router = createRouter({
       name: 'EditProfile',
       component: EditProfile,
     },
-    // Uncomment this section if you want to include AboutView
+
     // {
     //   path: '/about',
     //   name: 'about',
-    //   component: AboutView,
+    //   component: AboutView
     // },
   ],
-});
+})
 
-export default router;
+export default router
