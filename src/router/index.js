f3cae15380e54936ab2a380de005b472facdfda6
complatform <kalkidansol1623@gmail.com>
2025-03-27 16:06:53 +0300

    making corrections based on comments

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import HomeView from '../views/HomeView.vue'
 import RegistrationPage from '../views/RegistrationPage.vue'
 
 import UserRegistration1 from '../views/Users/UserRegistration1.vue'
+import FileUpload from '../views/Users/FileUpload.vue'
+
 import ForgotPassword from '../views/ForgotPassword.vue'
 import NextPage from '../views/Users/NextPage.vue'
 import SignIn from '../views/Users/SignIn.vue'
@@ -99,6 +101,12 @@ const router = createRouter({
       name: 'SignIn',
       component: SignIn,
     },
+    {
+      path: '/FileUpload',
+      name: 'FileUpload',
+      component: FileUpload,
+    },
+    
     {
       path: '/EditProfile',
       name: 'EditProfile',
