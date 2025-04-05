b7c46b4a75a9335558a34187099f90eedd478d1e
Tiegist Girma <tigistgirma40@gmail.com>
2025-04-05 16:42:05 +0300

    modifying based on comments

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,61 +1,80 @@
 <template>
-    <div class="mt-16">
-      <div id="map" style="height: 400px;"></div>
-      <button class="float-right cursor-pointer" @click="close">X</button>
-      <button class="bg-orange-400 text-white pointer-cursor hover:bg-amber-300 px-8 rounded-lg py-2" @click="saveLocation">Save Location</button>
-    </div>
-  </template>
-  
-  <script>
-  import L from 'leaflet';
-  
-  export default {
-    data() {
-      return {
-        map: null,
-        marker: null,
-        selectedLocation: {
-          lat: null,
-          lng: null,
-        },
-      };
+  <div class="mt-16">
+    <div id="map" style="height: 400px;"></div>
+    <button class="float-right cursor-pointer" @click="close">X</button>
+    <button class="bg-orange-400 text-white pointer-cursor hover:bg-amber-300 px-8 rounded-lg py-2" @click="saveLocation">Save Location</button>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+
+export default {
+  props: {
+    currentLocation: {
+      type: Object,
+      required: true,
     },
-    mounted() {
+  },
+  data() {
+    return {
+      map: null,
+      marker: null,
+      selectedLocation: {
+        lat: null,
+        lng: null,
+      },
+    };
+  },
+  mounted() {
+
+    if (this.currentLocation && this.currentLocation.lat && this.currentLocation.lng) {
       this.initMap();
+    } else {
+      console.error("Current location is not defined.");
+    }
+  },
+  methods: {
+    initMap() {
+      // Initialize the map centered on the current location
+      this.map = L.map('map').setView([this.currentLocation.lat, this.currentLocation.lng], 13);
+
+      // Add tile layer
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+      }).addTo(this.map);
+
+      // Add a marker for the current location
+      this.marker = L.marker([this.currentLocation.lat, this.currentLocation.lng]).addTo(this.map)
+        .bindPopup('You are here!')
+        .openPopup();
+
+      // Allow adding a new marker on map click
+      this.map.on('click', (e) => {
+        this.addMarker(e.latlng);
+      });
     },
-    methods: {
-      initMap() {
-        this.map = L.map('map').setView([51.505, -0.09], 13);
-  
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-        }).addTo(this.map);
-  
-        this.map.on('click', (e) => {
-          this.addMarker(e.latlng);
-        });
-      },
-      close() {
-        this.$emit('close');
-      },
-      addMarker(latlng) {
-        if (this.marker) {
-          this.marker.setLatLng(latlng);
-        } else {
-          this.marker = L.marker(latlng).addTo(this.map);
-        }
-        this.selectedLocation = latlng;
-      },
-      saveLocation() {
-        this.$emit('location-selected', this.selectedLocation);
-      },
+    close() {
+      this.$emit('close');
     },
-  };
-  </script>
-  
-  <style>
-  #map {
-    width: 100%;
-    height: 400px;
-  }
-  </style>
+    addMarker(latlng) {
+      if (this.marker) {
+        this.marker.setLatLng(latlng);
+      } else {
+        this.marker = L.marker(latlng).addTo(this.map);
+      }
+      this.selectedLocation = latlng;
+    },
+    saveLocation() {
+      this.$emit('location-selected', this.selectedLocation);
+    },
+  },
+};
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 400px;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="pt-5">
     <div class=" block md:hidden">
         <div class="flex w-screen">
             <img src="\logo.png" alt="" class="w-32 p-2 ">
@@ -9,21 +10,21 @@
         <div class=" w-1/2 md:w-2/3 mx-auto p-1 rounded-full  h-14 bg-[#C8EDF8]">
             <div class="flex">
             <div class="flex">
-            <img src="\logo.png" alt="" class="lg:w-24 lg:h-10 lg:mt-1 ml-4 md:w-16 md:h-8 md:mt-2">
+            <img src="\logo.png" alt="" class="lg:w-14 lg:h-10 lg:mt-1 ml-4 md:w-16 md:h-8 md:mt-2">
             <p class="lg:mt-2 ml-2 lg:text-lg md:text-[13px] md:font-semibold md:-ml-0.5 lg:mr-8">BIZEthio</p>
             </div>
-            <div class="flex lg:w-64 md:w-40 md:ml-5 lg:mx-auto mx-auto mt-3 md:text-[12px] lg:text-[16px]">
-                <p class="lg:mr-6 md:mr-4">Home</p>
-                <p class="lg:mr-6 md:mr-4">about us</p>
-                <p>contacts</p>
+            <div class="flex lg:w-3/4 md:w-40 lg:ml-40 font-semibold md:ml-5 lg:mx-auto mx-auto mt-3 md:text-[12px] lg:text-[16px]">
+                <router-link to="/UserLanding" class="lg:mr-32 md:mr-4">Home</router-link >
+                <router-link to="#" class="lg:mr-32  md:mr-4">about us</router-link>
+                <router-link to="#">contacts</router-link>
             </div>
-            <div class="mt-1 lg:-ml-8 md:-ml-4">
-                <i class="fa-solid fa-location-dot text-[#1B7590] lg:mr-6 md:mr-4 md:text-sm mt-2 lg:text-xl"></i>
-                <i class="fa-solid fa-user mr-4 text-gray-800 md:text-text-sm lg:text-xl"></i>
+            <div class="mt-3 lg:-ml-16 md:-ml-4">
+                <router-link to="/UserProfile" class="fa-solid fa-user mr-4 -ml-14 text-gray-800 md:text-text-sm lg:text-xl"></router-link>
             </div>
         </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
